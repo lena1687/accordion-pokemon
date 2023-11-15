aaa94d1a23d900d6b46ed723a99ce13a19776e68
eosedelnikova <eosedelnikova@luxoft.com>
2023-11-15 18:38:18 +0100

    Building a Basic Page Structure with Angular: stylization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {BrowserModule} from "@angular/platform-browser";
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
@@ -7,7 +8,6 @@ import {FooterComponent} from "./footer/footer.component";
 import {AsideComponent} from "./aside/aside.component";
 import {PokemonListComponent} from "./pokemon-list/pokemon-list.component";
 import {PokemonDetailsComponent} from "./pokemon-details/pokemon-details.component";
-import {HttpClientModule} from "@angular/common/http";
 import {LoaderComponent} from "./loader/loader.component";
 import {LoaderService} from "../services/loader.service";
 import {PokemonDataService} from "../services/pokemon.data.service";
